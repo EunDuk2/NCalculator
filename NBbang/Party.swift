78a02a4f681cd0a60ee362c82a4141de4f9a7fa4
eunsung <dmsqls6789@naver.com>
2023-03-29 09:57:23 +0900

    파티 클래스에서 add함수 두개 구현

--- NBbang/Party.swift
+++ NBbang/Party.swift
@@ -2,4 +2,12 @@
 class Party {
     var user:[User] = []
     var menu:[Menu] = []
+    
+    func addUser(name: String, phoneNumber: String?) {
+        user.append(User(name: name, phoneNumber: phoneNumber))
+    }
+    
+    func addMenu(name: String, price: Int) {
+        menu.append(Menu(name: name, price: price))
+    }
 }
